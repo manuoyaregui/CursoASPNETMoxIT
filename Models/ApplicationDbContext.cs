6808e43193ed76048dcd776db1d24ce0ca79a85b
Manuel Oyaregui <manu.oyaregui@gmail.com>
2023-04-11 11:30:08 -0300

    agrego EntityFramework y Configuro DB

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
